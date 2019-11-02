416fc8393a8b86b3d7384884eadb9c5c0e769486
Pavel Elfimov <pelfimov@yandex.ru>
2019-11-02 10:33:24 +0700

    add new data in mocks

--- src/mocks/offers.js
+++ src/mocks/offers.js
@@ -4,36 +4,41 @@ const offers = [
     cost: 120,
     name: `Beautiful & luxurious apartment at great location`,
     rating: 55,
-    type: `Apartment`
+    type: `Apartment`,
+    coordinates: [52.3909553943508, 4.85309666406198]
   },
   {
     isPremium: false,
     cost: 132,
     name: `Wood and stone place`,
     rating: 80,
-    type: `Apartment`
+    type: `Apartment`,
+    coordinates: [52.369553943508, 4.85309666406198]
   },
   {
     isPremium: true,
     cost: 120,
     name: `Canal View Prinsengracht`,
     rating: 55,
-    type: `Apartment`
+    type: `Apartment`,
+    coordinates: [52.3909553943508, 4.929309666406198]
   },
   {
     isPremium: true,
     cost: 80,
     name: `Nice, cozy, warm big bed apartment`,
     rating: 100,
-    type: `Apartment`
+    type: `Apartment`,
+    coordinates: [52.3809553943508, 4.939309666406198]
   },
-  {
-    isPremium: true,
-    cost: 120,
-    name: `Wood and stone place`,
-    rating: 88,
-    type: `Private room`
-  }
+  // {
+  //   isPremium: true,
+  //   cost: 120,
+  //   name: `Wood and stone place`,
+  //   rating: 88,
+  //   type: `Private room`,
+  //   coordinates: [52.3909553943508, 4.85309666406198]
+  // }
 ];
 
 export default offers;
