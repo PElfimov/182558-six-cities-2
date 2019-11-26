b3c4ee509d3f03f7c38cd9b7291f445221cde4d4
Pavel Elfimov <pelfimov@yandex.ru>
2019-11-26 22:26:36 +0700

    fix type bugs

--- src/components/main-page/city/city.jsx
+++ src/components/main-page/city/city.jsx
@@ -59,14 +59,23 @@ City.propTypes = {
     id: PropTypes.number,
     city: PropTypes.exact({
       name: PropTypes.string,
-      coordinates: PropTypes.arrayOf(PropTypes.number)
+      coordinates: PropTypes.arrayOf(PropTypes.number),
+      zoom: PropTypes.number,
     }),
     isPremium: PropTypes.bool,
+    isFavorite: PropTypes.bool,
     cost: PropTypes.number,
     name: PropTypes.string,
     rating: PropTypes.number,
-    type: PropTypes.oneOf([`Private room`, `Apartment`]),
-    coordinates: PropTypes.array
+    type: PropTypes.string,
+    coordinates: PropTypes.array,
+    previewImage: PropTypes.string,
+    images: PropTypes.arrayOf(PropTypes.string),
+    bedrooms: PropTypes.number,
+    maxAdults: PropTypes.number,
+    goods: PropTypes.arrayOf(PropTypes.string),
+    host: PropTypes.object,
+    description: PropTypes.string,
   })
 };
 
