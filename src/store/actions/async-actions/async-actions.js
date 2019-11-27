cc88d212947733d1a90c9e8c4fbd87f0f9769f29
Pavel Elfimov <pelfimov@yandex.ru>
2019-11-28 00:05:19 +0700

    not work

--- src/store/actions/async-actions/async-actions.js
+++ src/store/actions/async-actions/async-actions.js
@@ -21,14 +21,19 @@ const Operation = {
   favoriteHotelHandler: (id, status) => (dispatch, getState, api) => {
     const convertStatus = Number(status);
     return api.post(`/favorite/${id}/${convertStatus}`)
-    .then((response) => {
-      if (!response.error) {
-        const state = getState();
-        const {offers} = state.externalData;
-        const updatePlaces = offers.map((item) => item.id === response.data.id ? response.data : item);
-        dispatch(ActionCreator.loadOffers(updatePlaces));
-      }
-    });
+      .then((response) => {
+        if (!response.error) {
+          const state = getState();
+          const {cityOffers} = state.localData;
+
+
+          const updatePlaces = cityOffers
+            .map((item) => item.id === response.data.id ? (item.isFavorite = !item.isFavorite) : item);
+          console.log(updatePlaces);
+
+          dispatch(ActionCreator.setOffers(updatePlaces));
+        }
+      });
   }
 };
 
