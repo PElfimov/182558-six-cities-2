a40916fe4d93beb775ad7ef710ccb3b5ee998ac9
Pavel Elfimov <pelfimov@yandex.ru>
2019-12-02 22:17:19 +0700

    fix test

--- src/components/footer/footer.test.js
+++ src/components/footer/footer.test.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Footer from './footer';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 it(`HotelCard correctly renders after relaunch`, () => {
   const tree = renderer
-    .create(<Footer />)
+    .create(
+        <Router>
+          <Footer />
+        </Router>)
 
     .toJSON();
   expect(tree).toMatchSnapshot();
