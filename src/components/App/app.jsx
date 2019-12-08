2ab425862c9de1298c84078d2c3367d5dc99781a
Pavel Elfimov <pelfimov@yandex.ru>
2019-12-08 18:31:41 +0700

    add prpp types into app

--- src/components/App/app.jsx
+++ src/components/App/app.jsx
@@ -1,5 +1,6 @@
 import React, {PureComponent} from "react";
 import {connect} from "react-redux";
+import PropTypes from 'prop-types';
 import {ActionCreator} from "../../store/actions/action-creator/action-creator";
 import SignIn from '../sign-in/sign-in';
 import withSignIn from '../../hocs/with-sign-in/with-sign-in';
@@ -58,7 +59,33 @@ class App extends PureComponent {
   }
 }
 
-App.propTypes = {};
+App.propTypes = {
+  offers: PropTypes.arrayOf(PropTypes.exact({
+    id: PropTypes.number,
+    city: PropTypes.exact({
+      name: PropTypes.string,
+      coordinates: PropTypes.arrayOf(PropTypes.number),
+      zoom: PropTypes.number,
+    }),
+    isPremium: PropTypes.bool,
+    isFavorite: PropTypes.bool,
+    cost: PropTypes.number,
+    name: PropTypes.string,
+    rating: PropTypes.number,
+    type: PropTypes.string,
+    coordinates: PropTypes.array,
+    previewImage: PropTypes.string,
+    images: PropTypes.arrayOf(PropTypes.string),
+    bedrooms: PropTypes.number,
+    maxAdults: PropTypes.number,
+    goods: PropTypes.arrayOf(PropTypes.string),
+    host: PropTypes.object,
+    description: PropTypes.string,
+  })
+  ).isRequired,
+  changeCity: PropTypes.func,
+  isLogin: PropTypes.bool
+};
 
 const mapStateToProps = (state, ownProps) =>
   Object.assign({}, ownProps, {
